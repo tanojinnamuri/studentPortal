edf8a572b95cfd10d2d8319dcfa20d63a8ad8bdc
Muhammad Hassan Mustafa <tinnamuri@albany.edu>
2023-02-23 02:19:09 +0500

    added file conversion to base64

--- client/src/components/ProjectModal.jsx
+++ client/src/components/ProjectModal.jsx
@@ -21,12 +21,59 @@ class ProjectModal extends Component {
     teamMembers: "",
     department: "ComputerScience",
     year: "",
+    file: null,
   };
   handleClose = () => {
     this.setState({ show: false });
   };
   handleShow = () => {
     this.setState({ show: true });
+  };
+
+  getBase64 = (file) => {
+    return new Promise((resolve) => {
+      let fileInfo;
+      let baseURL = "";
+      // Make new FileReader
+      let reader = new FileReader();
+
+      // Convert the file to base64 text
+      reader.readAsDataURL(file);
+
+      // on reader load somthing...
+      reader.onload = () => {
+        // Make a fileInfo Object
+        console.log("Called", reader);
+        baseURL = reader.result;
+        console.log(baseURL);
+        resolve(baseURL);
+      };
+      console.log(fileInfo);
+    });
+  };
+
+  handleFileInputChange = (e) => {
+    console.log(e.target.files[0]);
+    let { file } = this.state;
+
+    file = e.target.files[0];
+
+    this.getBase64(file)
+      .then((result) => {
+        file["base64"] = result;
+        console.log("File Is", file);
+        this.setState({
+          poster: result,
+          file,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    this.setState({
+      file: e.target.files[0],
+    });
   };
 
   handleSubmit(e) {
@@ -135,7 +182,7 @@ class ProjectModal extends Component {
                   autoFocus
                   required
                   name="poster"
-                  onChange={(e) => this.handleChange(e)}
+                  onChange={(e) => this.handleFileInputChange(e)}
                 />
               </Form.Group>
               <Form.Group
