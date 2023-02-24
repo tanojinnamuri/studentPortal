19a0bde2e44c9f4de087efb73b828b3bf0e936b1
Muhammad Hassan Mustafa <tinnamuri@albany.edu>
2023-02-24 23:51:58 +0500

    fix the add project issue in case of image

--- app.js
+++ app.js
@@ -4,20 +4,23 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 const { handleErrors } = require("./helpers/error");
-var cors = require('cors')
+var cors = require("cors");
+const bodyParser = require("body-parser");
 
 var indexRouter = require("./routes/index");
 var apiRouter = require("./routes/api");
 
 var app = express();
-app.use(cors())
+app.use(cors());
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
 app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
